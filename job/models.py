2bddb7f70b444f1f1bc77eadda08b6282f44b47a
Your Name <you@example.com>
2023-06-18 18:11:22 +0200

    edit1 commit

--- job/models.py
+++ job/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+#from django field on website
+JOBTYPE=(('FULL','FULL'),('PART','PART'))
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class job(models.Model):
+
+    titel=models.CharField(max_length =100)
+    job_tybe=models.CharField(max_length=15 ,choices=JOBTYPE)
+    description=models.TextField(max_length=1000)
+    publishedat=models.DateTimeField(auto_now=True)
+    Vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=0)
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
+#for return with titel after saving job
+    def __str__(self):
+         return self.titel
+
--- job/views.py
+++ job/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import job
 # Create your views here.
+
+def job_list(request):
+    joblist=job.objects.all()
+    context={'jobs':joblist}
+    return render(request,'jobs/job_list.html',context)
+
+def job_des(request,id):
+    jobdes=job.objects.get(id=id)
+    context={'job':jobdes}
+    return render(request,'jobs/job_des.html',context)
